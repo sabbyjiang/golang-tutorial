89e4b9a2d0c1c37d3ca6096a00d60d9bc12147cb
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-06-06 13:35:17 -0400

    maps

--- answers.go
+++ answers.go
@@ -24,3 +24,17 @@ func Pic(dx, dy int) [][]uint8 {
 	}
 	return ret
 }
+
+// Exercise: Maps
+// Implement WordCount. It should return a map of the counts of each “word” in the string s. The wc.Test function runs a test suite against the provided function and prints success or failure.
+
+func WordCount(s string) map[string]int {
+	counts := make(map[string]int)
+	splits := strings.Fields(s)
+	
+	for i := 0; i < len(splits); i++ {
+		fmt.Printf("%q", splits[i]);
+		counts[splits[i]] = strings.Count(s, splits[i])
+	}
+	return counts
+}
