74edc03616dca25da9e0f9b2957653864ab4c179
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2017-06-06 12:30:10 -0400

    slices exercise

--- answers.go
+++ answers.go
@@ -7,3 +7,20 @@ func Sqrt(x float64) float64 {
 	}
 	return z
 }
+
+// Exercise: Slices
+
+func Pic(dx, dy int) [][]uint8 {
+	// creates a slice of of length dy with slices that contain uint8 types
+	ret := make([][]uint8, dy)
+
+	// at each slice within ret, make a slice with length dx
+	for i := 0; i < dy; i++ {
+		ret[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			// Within each element inside this matrix, create a value
+			ret[i][j] = uint8(i^j + 2*i*j)
+		}
+	}
+	return ret
+}
